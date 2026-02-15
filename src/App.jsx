19ea3db5d9cebcecbbb8a853e1a40e75900c69e6
Ritesh Maurya <riteshmaury97@gmail.com>
2026-02-16 01:47:56 +0530

    Model Switcher has been Added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import ProductViewer from "./Components/ProductViewer";
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
