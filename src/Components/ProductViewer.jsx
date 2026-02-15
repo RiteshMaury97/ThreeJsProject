4c18014b6721b7e93893913ca5da3bf4b646a172
Ritesh Maurya <riteshmaury97@gmail.com>
2026-02-16 01:11:04 +0530

    lighting is Setup

--- src/Components/ProductViewer.jsx
+++ src/Components/ProductViewer.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import useMacbookStore from "../store";
 import clsx from "clsx";
 import { Canvas } from "@react-three/fiber";
+import StudioLights from "../Components/StudioLights";
 // import { Box, OrbitControls } from "@react-three/drei";
 import MacbookModel14 from "./models/Macbook-14";
 import { OrbitControls } from "@react-three/drei";
@@ -58,7 +59,7 @@ const ProductViewer = () => {
         id="canvas"
         camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100 }}
       >
-        <ambientLight intensity={1} />
+        <StudioLights />
         <MacbookModel14 scale={0.06} position={[0, 0, 0]} />
         <OrbitControls enableZoom={false} />
       </Canvas>
